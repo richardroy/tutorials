ebdcd3201577cd07dce137c95f712a9ccada881b
richardroy <richardroygibson@gmail.com>
2019-03-11 13:06:04 +0700

    moved password into a env

--- scotch/updated/config.js
+++ scotch/updated/config.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
+
 module.exports = {
     'secret': 'ilovescotchyscotch',
-    'database': 'mongodb://richardgibson_me_projects_app:RCRd3kGFd3j2KT6Lw4SYqI3s@ds163835.mlab.com:63835/richardgibson_me_projects'
+    'database': `mongodb://richardgibson_me_projects_app:${process.env.MONGODB_PASSWORD}@ds163835.mlab.com:63835/richardgibson_me_projects`
 };
