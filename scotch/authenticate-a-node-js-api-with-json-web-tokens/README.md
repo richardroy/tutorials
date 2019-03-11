b78d3106ed32e95839269ac0e2e55a44726a701a
richardroy <richardroygibson@gmail.com>
2019-03-11 13:00:05 +0700

    removed node_modules, updated README

--- scotch/authenticate-a-node-js-api-with-json-web-tokens/README.md
+++
@@ -1,1 +0,0 @@
-Source: https://scotch.io/tutorials/authenticate-a-node-js-api-with-json-web-tokens
--- scotch/authenticate-a-node-js-api-with-json-web-tokens/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    'secret': 'ilovescotchyscotch',
-    'database': 'mongodb://richardgibson_me_projects_app:RCRd3kGFd3j2KT6Lw4SYqI3s@ds163835.mlab.com:63835/richardgibson_me_projects'
-};
