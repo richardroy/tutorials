2719c167b773ca5ab833dad767c4e5efd9bf6057
richardroy <richardroygibson@gmail.com>
2019-03-11 12:43:45 +0700

    complete the authenticate-a-node-js-api-with-json-web-tokens tutorial

---
+++ scotch/authenticate-a-node-js-api-with-json-web-tokens/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'secret': 'ilovescotchyscotch',
+    'database': 'mongodb://richardgibson_me_projects_app:RCRd3kGFd3j2KT6Lw4SYqI3s@ds163835.mlab.com:63835/richardgibson_me_projects'
+};
